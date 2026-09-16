06969e1f13473ad3fff4b1bab0d4f52c891315f7
Alex <alex_franca150@estudante.sesisenai.org.br>
2026-09-16 16:17:39 -0300

    sim

--- 01-js/src/App.jsx
+++ 01-js/src/App.jsx
@@ -21,17 +21,72 @@ function App() {
     
     let IMC = Number (peso/(altura*altura))
 
-    alert("seu IMC: "+IMC)
+    alert("seu IMC: "+IMC.toFixed(2))
   }
+ function calcularpontos(){
+   let vitorias = Number(prompt("Número de vitórias"))
+   let empates = Number(prompt("Número de empates"))
+   
+   let pontos = vitorias*3 + empates;
 
+   alert("o time teu tem "+pontos+" pontos")
+}
+ function calcularsapatos(){
+  let sapatos = Number(prompt("qual o valor do sapatos que você vai trocar:"))
+  let trocas = Number(prompt("quantos calçados você quer trocar:"))
+
+  let vales = trocas*sapatos
+
+  alert("valor total de troca: R$"+vales.toFixed(2))
+
+ }
+ function calcularDEVS(){
+  let CLT = Number(prompt("quantos CLTs temos:"))
+  let estagiarios = Number(prompt("quantos estagiarios temos:"))
+  let PJ = Number(prompt("quantos PJs temos:"))
+
+  let total = CLT+estagiarios+PJ
+
+  alert("Temos o total de "+total+" devs")
+ }
+ function calcularLaranjas(){
+  let laranjasI = Number(prompt("quantas laranjas temos no incio:"))
+  let laranjasF = Number(prompt("quantas laranjas temos no final:"))
+
+  let laranjaV = laranjasI-laranjasF
+
+  alert("o total de laranjas vendidas, foi "+laranjaV)
+ }
+ function CalcularDivida(){
+   let custoM = Number(prompt("qual o custo mensal que a igreja tem:"))
+   let dízimo = Number(prompt("qual o valor arrecadado pelo dízimo"))
+
+   let divida_atualizada = custoM-dízimo
+
+   if(custoM<dízimo){
+    alert("a divida esta quitada")
+   }
+   else{
+    alert("ainda resta o total de R$"+divida_atualizada.toFixed(2)+" a ser pago")
+   }
+ }
   return (
    <div className="cont-app">
     <h1>Javascript no React</h1>
-    <button onClick={testar}>testar</button>
-    <button onClick={calcularMedia}>média</button>
+
+    <h2>Exercicios Supimpas</h2>
+
+   <button onClick={calcularpontos}>Campeonato</button>
+   <button onClick={calcularsapatos}>Troca pé pequeno</button>
+   <button onClick={calcularDEVS}>Força Devs</button>
+   <button onClick={calcularLaranjas}>Pomar</button>
+   <button onClick={CalcularDivida}>Divida Sagrada</button>
+   <hr/>
+
+    <button onClick={testar}>Testar</button>
+    <button onClick={calcularMedia}>Média</button>
     <button onClick={calcularIMC}>IMC</button>
    </div>
   )
 }
-
 export default App
