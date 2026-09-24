bad3802427906130f0db197e2dc56852e179ed1b
Alex <alex_franca150@estudante.sesisenai.org.br>
2026-09-24 17:14:27 -0300

    sim

---
+++ 02-studio_alfa/src/components/Main/Main.jsx
@@ -0,0 +1,24 @@
+import "./Main.css";
+
+function Main() {
+  return (
+    <main className="main">
+      <section className="hero">
+        <h1>Criamos sites que funcionam com perfeição</h1>
+        <p>
+          Layouts responsivos, rápidos e acessíveis para o seu negócio crescer
+        </p>
+        <div className="hero-buttons">
+          <a href="#orcamentos" className="btn-primary">
+            Peça um orçamento
+          </a>
+          <a href="#portifolio" className="btn-secondary">
+            Ver portifólio
+          </a>
+        </div>
+      </section>
+      <section className="service"></section>
+    </main>
+  );
+}
+export default Main;
